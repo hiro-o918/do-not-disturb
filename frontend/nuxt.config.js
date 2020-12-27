509ecbc8533216ac1643d454d3651f83dfa1d261
Hironoiri Yamamoto <mr.nikoru918@gmail.com>
2020-12-27 22:59:57 +0900

    Add frontend

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'frontend',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Frontend for do not disturb project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/vuetify',
+  ],
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  axios: {}
+}
+
---
+++ frontend/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">Do Not Distub</h1>
+      <div class="buttons">
+        <div class="button">
+          <v-btn elevation="2" large v-on:click="doNotDisturb"
+            >Do Not Distub</v-btn
+          >
+        </div>
+        <div class="button">
+          <v-btn elevation="2" large v-on:click="release">Release</v-btn>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+
+export default {
+  // TODO: set hostname by an enviroment variable
+  methods: {
+    doNotDisturb: () => {
+      axios.post("http://localhost:8000/do-not-disturb");
+    },
+    release: () => {
+      axios.post("http://localhost:8000/release");
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.buttons {
+  display: flex;
+  margin: 20px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.button {
+  margin: 0 20px;
+}
+</style>
+
